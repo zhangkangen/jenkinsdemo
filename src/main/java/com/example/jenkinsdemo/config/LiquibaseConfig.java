b9b30fd319976aeeda2749f091f49a4e6b2940c2
zhangxiaokang <zhangxiaokang@bainuo.com>
2018-03-24 18:10:39 +0800

    使用liquibase

--- src/main/java/com/example/jenkinsdemo/config/LiquibaseConfig.java
+++ src/main/java/com/example/jenkinsdemo/config/LiquibaseConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class LiquibaseConfig {
 
-    @Bean
+    //@Bean
     public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
